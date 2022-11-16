d20fe1d05e291bd96190320f1955a7b8f8f9092e
Adrienne <adrienne@Zipcoders-MacBook-Pro-101.local>
2022-11-16 01:14:31 -0500

    review table

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,54 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
-    }
 
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 1) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
     }
 
-
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        return getExponentiations(start, stop, step, 1);
     }
 
     public static String getRange(int start) {
-        return null;
+        return getRange(0, start, 1);
     }
+
 
     public static String getRange(int start, int stop) {
-        return null;
-    }
+        return getRange(start, stop, 1);
 
+    }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 1);
     }
 
-
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i += step) {
+            sb.append(Math.pow(i, exponent));
+        }
+        return sb.toString();
     }
 }
