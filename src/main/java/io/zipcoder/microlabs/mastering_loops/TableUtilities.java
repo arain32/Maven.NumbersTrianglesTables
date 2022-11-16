c8272846631298b393d7b37bc306512218a2c04c
Adrienne <adrienne@Zipcoders-MacBook-Pro-101.local>
2022-11-16 14:04:42 -0500

    needs review

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,17 +2,14 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-
         return getMultiplicationTable(5);
     }
-
     public static String getLargeMultiplicationTable() {
-
         return getMultiplicationTable(10);
     }
-
     public static String getMultiplicationTable(int tableSize) {
-        return getMultiplicationTable( 20);
+        //return getMultiplicationTable( 20);
+        return null;
     }
 }
 
